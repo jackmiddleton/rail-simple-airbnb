4a2205827b4e6b4fc6975cba11aaddc93e06b33e
jack middleton hello@jackmiddleton.co <hello@jackmiddleton.co>
2021-04-25 13:32:45 +0100

    rails new with front-end setup

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,37 @@
 class FlatsController < ApplicationController
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    # save the flat
+    @flat.save
+    #redirect back to index page
+    redirect_to flats_path
+  end
+
+  def edit
+  end
+
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+
+  def flat_params
+    #manadatory method to safeguard params.
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
 end

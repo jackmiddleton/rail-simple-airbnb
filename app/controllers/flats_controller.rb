e640e9c7b7e6328105f73e242295b00b0e7da09a
jack middleton hello@jackmiddleton.co <hello@jackmiddleton.co>
2021-04-25 15:16:23 +0100

    all

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,7 +1,14 @@
 class FlatsController < ApplicationController
+before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
   def index
+    if params[:query].present?
+      @query = params[:query]
+      @flats = Flat.where("name LIKE ?","%#{params[:query]}%")
+      # Preventing SQL Injection and Database error for unknown characters
+    else
     @flats = Flat.all
+    end
   end
 
   def show
@@ -14,19 +21,28 @@ class FlatsController < ApplicationController
   def create
     @flat = Flat.new(flat_params)
     # save the flat
-    @flat.save
+    if @flat.save
     #redirect back to index page
     redirect_to flats_path
+    else
+    render :new
+    end
   end
 
   def edit
   end
 
+  def update
+    @flat.update(flat_paramss)
+    redirect_to flat_path(@flat)
+  end
 
   def destroy
     @flat.destroy
     redirect_to flats_path
   end
+
+  private
 
   def find_flat
     @flat = Flat.find(params[:id])
@@ -34,4 +50,6 @@ class FlatsController < ApplicationController
   def flat_params
     #manadatory method to safeguard params.
     params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
 end
